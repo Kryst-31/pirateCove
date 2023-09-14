a609a8d0542e85adaed90424f521ed92888728e8
Kryst-31 <connorbg99@gmail.com>
2023-09-13 22:04:02 -0400

    add start of app

---
+++ src/app/menu/menu.component.ts
@@ -0,0 +1,40 @@
+import { Component } from '@angular/core';
+import { recipe } from './recipeModels/recipe';
+
+@Component({
+  selector: 'app-menu',
+  templateUrl: './menu.component.html',
+  styleUrls: ['./menu.component.css']
+})
+export class MenuComponent {
+
+  items: recipe[] = []
+
+  constructor() {
+    this.generateMenuItems();
+  }
+
+  private generateMenuItems() {
+    let spaghetti: recipe = {
+      name: "Spaghetti",
+      summary: "An easy to make spaghetti with spicy sausage and mushroom spaghetti sauce!",
+      ingredients: [
+        "Spaghetti Noodles",
+        "Spaghetti Sauce",
+        "Bob Evans Sausage"
+      ],
+      instructions: [
+        "Heat a pot of water to a boil",
+        "Add noodles and cook as instructed on box",
+        "While cooking the noodles, brown the Bob Evans sausage",
+        "Once the sausage and noodles are done cooking, add both contents to the same pot and add the spaghetti sauce!"
+      ],
+      image: "assets/MenuImages/Spaghetti.jpg"
+    }
+
+    this.items.push(spaghetti);
+    this.items.push(spaghetti);
+    this.items.push(spaghetti);
+  }
+
+}
---
+++ src/app/menu/recipeModels/recipe.ts
@@ -0,0 +1,8 @@
+export type recipe = {
+    name: string;
+    cookTime?: string;
+    summary?: string;
+    ingredients: string[];
+    instructions: string[];
+    image?: string;
+}
