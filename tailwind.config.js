49a7459a19a469635069d624ce17bb59cd3e617b
boucherg <boucherg@msu.edu>
2023-10-02 23:02:02 -0400

    fix compiling issue for module building

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,13 +3,9 @@ module.exports = {
   content: [
     "./src/app/app.component.{html,ts}",
     "./src/app/menu/menu.component.{html,ts}",
-<<<<<<< Updated upstream
     "./src/app/menu/components/menu-item/menu-item.component.{html,ts}",
     "./src/app/home/home.component.{html,ts}",
-=======
-    "./src/app/home/home.component.{html,ts}",
-    "./src/app/home/dungeonGenerator.component.{html,ts}",
->>>>>>> Stashed changes
+    "./src/app/dungeon-display/dungeon-display.component.{html,ts}",
   ],
   theme: {
     extend: {},
